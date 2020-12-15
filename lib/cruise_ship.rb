aa87cdf42e4ce03ba908eb2eaa5df7cf45f054d2
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 15:05:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each do |suite, name|
+  passengers.collect do |suite, name|
     if suite == 'suite_a' && name.firt == "A"
       return name
     end
